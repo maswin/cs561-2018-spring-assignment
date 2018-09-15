470fc9da02d46592913044a208281bb1183089d3
Alagappan Maruthappan <aswin@Alagappans-MacBook-Pro.local>
2018-09-14 20:38:10 -0700

    exhaustive solution

--- hw1cs561f2018.py
+++ hw1cs561f2018.py
@@ -1,34 +1,7 @@
-import sys
-
-INPUT_FILE_NAME = "input.txt"
-OUTPUT_FILE_NAME = "output.txt"
+INPUT_FILE_NAME = "input3.txt"
+OUTPUT_FILE_NAME = "output3.txt"
 ANS = []
-
-
-def visualize_scooters(grid_size, all_scooter_positions):
-    grids = [["****"] * grid_size for x in range(grid_size)]
-
-    def refresh_grids():
-        for x in range(grid_size):
-            for y in range(grid_size):
-                grids[x][y] = "****"
-
-    scooter_index = 1
-    for scooter_positions in all_scooter_positions:
-        refresh_grids()
-        print("")
-        print("Scooter : " + str(scooter_index))
-        print("")
-        for timestamp in range(12):
-            x, y = scooter_positions[timestamp]
-            if grids[x][y] == '****':
-                grids[x][y] = ""
-            grids[x][y] += (" T-" + str(timestamp + 1))
-        for grid in grids:
-            print("---------------------------------------------------------------------")
-            print("\t | \t".join(grid))
-            print("---------------------------------------------------------------------")
-        scooter_index += 1
+max_collected_points = 0
 
 
 def visualize_grids(grids):
@@ -82,38 +55,37 @@ def is_same_row(new_police_coordinate, coordinate):
 
 
 def place_police_officers_util(grid_size, points_grid, placed_police_officers_coordinate, number_of_police_officers,
-                               row):
-    # If unplaced police officers are more than remaining column, we can't proceed with this solution
+                               row, collected_points):
     remaining_police = number_of_police_officers - len(placed_police_officers_coordinate)
-    if remaining_police > (grid_size - row):
-        return 1 - sys.maxsize
+    if remaining_police == 0:
+        global max_collected_points
+        global ANS
+        if collected_points > max_collected_points:
+            max_collected_points = collected_points
+            ANS = placed_police_officers_coordinate[:]
 
-    max_points = 0
-    for column in range(grid_size):
-        if remaining_police > 0 and row < grid_size and is_position_safe(placed_police_officers_coordinate,
-                                                                         (row, column)):
-            # Search without placing a police here
-            points = place_police_officers_util(grid_size, points_grid, placed_police_officers_coordinate,
-                                                number_of_police_officers, row + 1)
-            if points > max_points:
-                max_points = points
-
+    if 0 < remaining_police <= (grid_size - row) and row < grid_size:
+        for column in range(grid_size):
             # Search by placing a police here
-            placed_police_officers_coordinate.append((row, column))
-            points = points_grid[row][column] + place_police_officers_util(grid_size, points_grid,
-                                                                           placed_police_officers_coordinate,
-                                                                           number_of_police_officers, row + 1)
-            if points > max_points:
-                max_points = points
-            placed_police_officers_coordinate.pop()
+            if is_position_safe(
+                    placed_police_officers_coordinate,
+                    (row, column)):
+                placed_police_officers_coordinate.append((row, column))
+                place_police_officers_util(grid_size, points_grid,
+                                           placed_police_officers_coordinate,
+                                           number_of_police_officers, row + 1,
+                                           collected_points + points_grid[row][column])
+                placed_police_officers_coordinate.pop()
 
-    return max_points
+            # Search without placing a police here
+            place_police_officers_util(grid_size, points_grid, placed_police_officers_coordinate,
+                                       number_of_police_officers, row + 1, collected_points)
 
 
 def place_police_officers(grid_size, points_grid, number_of_police_officers):
     placed_police_officers_coordinate = []
     return place_police_officers_util(grid_size, points_grid, placed_police_officers_coordinate,
-                                      number_of_police_officers, 0)
+                                      number_of_police_officers, 0, 0)
 
 
 def write_result_to_output(result):
@@ -122,9 +94,20 @@ def write_result_to_output(result):
     output_file.close()
 
 
-def assert_output(max_points):
+def print_ans(pg):
     print("Answer : ")
-    # print(ANS)
+    print(ANS)
+    print(" + ".join([str(pg[a[0]][a[1]]) for a in ANS]) + " = " + str(sum([pg[a[0]][a[1]] for a in ANS])))
+    n = len(pg)
+    grids = [[" X "] * n for x in range(n)]
+    for a in ANS:
+        grids[a[0]][a[1]] = " P "
+    for grid in grids:
+        print(" | ".join(grid))
+
+
+def assert_output(max_points, pg):
+    print_ans(pg)
     print("Actual output : " + str(max_points))
     f = open(OUTPUT_FILE_NAME, "r")
     expected_output = int(f.readline())
@@ -135,12 +118,11 @@ def assert_output(max_points):
 
 def run_homework():
     (grid_size, number_of_police_officers, all_scooter_positions) = get_input()
-    # visualize_scooters(grid_size, all_scooter_positions)
     points_grid = construct_points_grid(grid_size, all_scooter_positions)
     visualize_grids(points_grid)
-    max_points = place_police_officers(grid_size, points_grid, number_of_police_officers)
-    assert_output(max_points)
-    # write_result_to_output(max_points)
+    place_police_officers(grid_size, points_grid, number_of_police_officers)
+    assert_output(max_collected_points, points_grid)
+    # write_result_to_output(max_collected_points)
 
 
 if __name__ == "__main__":
