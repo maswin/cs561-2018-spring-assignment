fbaf00261f754d0c2d20b1da8a91cc7dfe2dec7c
Alagappan Maruthappan <aswin@Alagappans-MacBook-Pro.local>
2018-09-17 16:36:18 -0700

    refactored code, removed unwanted variables

--- hw1cs561f2018.py
+++ hw1cs561f2018.py
@@ -4,13 +4,12 @@ ANS = []
 MAX_COLLECTED_POINTS = 0
 MAX_ACHIEVABLE = [[]]
 SORTED_COLUMNS = [[]]
-SORTED_ROWS = []
 SLASH = []
 BACK_SLASH = []
 COLS = []
 
 
-def visualize_grids(grids):
+def visualize_grids_util(grids):
     for grid in grids:
         print(grid)
 
@@ -39,7 +38,7 @@ def construct_points_grid(grid_size, all_scooter_positions):
     return points_grid
 
 
-def is_position_safe_opt(coordinate, grid_size):
+def is_position_safe(coordinate, grid_size):
     return COLS[coordinate[1]] and SLASH[coordinate[0] - coordinate[1] + grid_size - 1] and BACK_SLASH[
         coordinate[0] + coordinate[1]]
 
@@ -56,86 +55,63 @@ def make_un_safe(coordinate, grid_size):
     BACK_SLASH[coordinate[0] + coordinate[1]] = False
 
 
-def is_position_safe(placed_police_officers_coordinate, new_police_coordinate):
-    for coordinate in placed_police_officers_coordinate:
-        if is_same_row(new_police_coordinate, coordinate) or \
-                is_same_col(new_police_coordinate, coordinate) or \
-                is_same_diagnol(new_police_coordinate, coordinate):
-            return False
-    return True
-
-
-def is_same_diagnol(new_police_coordinate, coordinate):
-    return abs(coordinate[0] - new_police_coordinate[0]) == abs(coordinate[1] - new_police_coordinate[1])
-
-
-def is_same_col(new_police_coordinate, coordinate):
-    return coordinate[1] == new_police_coordinate[1]
-
-
-def is_same_row(new_police_coordinate, coordinate):
-    return coordinate[0] == new_police_coordinate[0]
-
-
-def place_police_officers_util(grid_size, points_grid, placed_police_officers_coordinate, number_of_police_officers,
-                               row, collected_points):
-    remaining_police = number_of_police_officers - len(placed_police_officers_coordinate)
-
+def place_police_officer_util(grid_size, points_grid, remaining_police, row, collected_points):
     global MAX_COLLECTED_POINTS
+
     if row < grid_size and (collected_points + MAX_ACHIEVABLE[row][remaining_police - 1]) <= MAX_COLLECTED_POINTS:
         return
 
     if remaining_police == 0:
-        # global ANS
         if collected_points > MAX_COLLECTED_POINTS:
             MAX_COLLECTED_POINTS = collected_points
-            # ANS = placed_police_officers_coordinate[:]
 
     if 0 < remaining_police <= (grid_size - row) and row < grid_size:
+        # Search without placing a police here
+        place_police_officer_util(grid_size, points_grid, remaining_police, row + 1, collected_points)
+
+        # Search by placing a police at each column sorted by maximum points
         for column_index in range(grid_size):
             column = SORTED_COLUMNS[row][column_index]
-            # Search by placing a police here
             new_coord = (row, column)
-            if is_position_safe_opt(new_coord, grid_size):
-                placed_police_officers_coordinate.append(new_coord)
+            if is_position_safe(new_coord, grid_size):
+                remaining_police -= 1
                 make_un_safe(new_coord, grid_size)
-                place_police_officers_util(grid_size, points_grid,
-                                           placed_police_officers_coordinate,
-                                           number_of_police_officers, row + 1,
-                                           collected_points + points_grid[row][column])
+                place_police_officer_util(grid_size, points_grid, remaining_police, row + 1,
+                                          collected_points + points_grid[row][column])
                 make_safe(new_coord, grid_size)
-                placed_police_officers_coordinate.pop()
-
-        # Search without placing a police here
-        place_police_officers_util(grid_size, points_grid, placed_police_officers_coordinate, number_of_police_officers,
-                                   row + 1, collected_points)
+                remaining_police += 1
 
 
-def place_police_officers(grid_size, points_grid, number_of_police_officers):
-    global MAX_ACHIEVABLE
+def construct_max_achievable(number_of_police_officers, points_grid, grid_size):
     max_per_column = [max(grid) for grid in points_grid]
-
+    global MAX_ACHIEVABLE
     MAX_ACHIEVABLE = [[0] * number_of_police_officers for x in range(grid_size)]
     for row in range(grid_size):
         for remaining_police in range(number_of_police_officers):
             MAX_ACHIEVABLE[row][remaining_police] = sum(
-                sorted(max_per_column[row:], reverse=True)[:remaining_police+1])
+                sorted(max_per_column[row:], reverse=True)[:remaining_police + 1])
 
-    global SORTED_COLUMNS
-    SORTED_COLUMNS = get_sorted_columns(points_grid, grid_size)
 
+def construct_position_arrays(grid_size):
     global SLASH
     SLASH = [True] * ((2 * grid_size) - 1)
-
     global BACK_SLASH
     BACK_SLASH = [True] * ((2 * grid_size) - 1)
-
     global COLS
     COLS = [True] * grid_size
 
-    placed_police_officers_coordinate = []
-    return place_police_officers_util(grid_size, points_grid, placed_police_officers_coordinate,
-                                      number_of_police_officers, 0, 0)
+
+def construct_sorted_columns(points_grid, grid_size):
+    global SORTED_COLUMNS
+    SORTED_COLUMNS = [sorted(range(grid_size), key=lambda k: grid[k], reverse=True) for grid in points_grid]
+
+
+def place_police_officers(grid_size, points_grid, number_of_police_officers):
+    construct_max_achievable(number_of_police_officers, points_grid, grid_size)
+    construct_position_arrays(grid_size)
+    construct_sorted_columns(points_grid, grid_size)
+
+    return place_police_officer_util(grid_size, points_grid, number_of_police_officers, 0, 0)
 
 
 def write_result_to_output(result):
@@ -144,17 +120,12 @@ def write_result_to_output(result):
     output_file.close()
 
 
-def get_sorted_columns(points_grid, grid_size):
-    return [sorted(range(grid_size), key=lambda k: grid[k], reverse=True) for grid in points_grid]
-
-
 def get_sorted_rows(grid_size):
-    return sorted(range(grid_size), key=lambda k: MAX_ACHIEVABLE[k], reverse=True)
+    global SORTED_COLUMNS
+    SORTED_COLUMNS = sorted(range(grid_size), key=lambda k: MAX_ACHIEVABLE[k], reverse=True)
 
 
 def print_ans(pg):
-    print("Sorted rows : " + str(SORTED_ROWS))
-    print("Sorted columns : " + str(SORTED_COLUMNS))
     print("Answer : ")
     print(ANS)
     print(" + ".join([str(pg[a[0]][a[1]]) for a in ANS]) + " = " + str(sum([pg[a[0]][a[1]] for a in ANS])))
@@ -172,14 +143,14 @@ def assert_output(max_points, pg):
     f = open(OUTPUT_FILE_NAME, "r")
     expected_output = int(f.readline())
     print("Expected output : " + str(expected_output))
-    assert max_points == expected_output
+    # assert max_points == expected_output
     f.close()
 
 
 def run_homework():
     (grid_size, number_of_police_officers, all_scooter_positions) = get_input()
     points_grid = construct_points_grid(grid_size, all_scooter_positions)
-    # visualize_grids(points_grid)
+    # visualize_grids_util(points_grid)
     place_police_officers(grid_size, points_grid, number_of_police_officers)
     # assert_output(MAX_COLLECTED_POINTS, points_grid)
     write_result_to_output(MAX_COLLECTED_POINTS)
